c91ad4310dbb396bf145134bdb546aee3067c4e2
hardikbh <91249866+hardikbh@users.noreply.github.com>
2024-01-07 19:35:23 +0000

    commit

---
+++ 01_basics/conversionAndoperation.js
@@ -0,0 +1,22 @@
+let score = ""
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//"33" => 33
+// "33abc" => Nan
+// null =>0
+// true=1,false=0
+// undefined => Nan
+//""=>0
+let isLoggedIn=0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn)
+
+// 1=> true
+// 0=>false
+// ""=>flase
+// "hi"=>true
+
+//console.log(2**3)
