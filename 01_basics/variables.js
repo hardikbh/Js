2c71e0f90f0348818ea9e3a0fdf299c8f7589fea
hardikbh <91249866+hardikbh@users.noreply.github.com>
2024-01-07 17:23:38 +0000

    coomint config

---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountId=9927
+let accountEmail="hardik@gmail.com"
+var accountPassword="1234"
+accountCity="jaipur"
+let accountState
+// accountId=9925 //not allowed
+
+accountEmail="hardikbh@gmail.com"
+accountPassword="12345"
+accountCity="raipur"
+console.log(accountId);
+/*
+Prefer not to use var because of block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
