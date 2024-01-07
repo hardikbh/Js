9dabce3d218d9b33be4a72bc65a4890d1deddb53
hardikbh <91249866+hardikbh@users.noreply.github.com>
2024-01-07 17:49:26 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,20 @@
+//"use strict" // treat all Js code as newer version
+
+//alert(3 +3) // we are using nodejs, not browser
+
+let name = "hardik"
+let age = 16
+let isLoggedIn = false
+let state = null
+/*
+  name =>2 to power 53
+  bigint
+  string =>""
+  boolean  => true/false
+  null =>standalone value
+  undefined =>
+  symbol=> unique
+  object
+*/
+console.log(typeof undefined) //undefined
+console.log(typeof null) // object
