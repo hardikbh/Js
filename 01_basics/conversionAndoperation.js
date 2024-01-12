199bd74a7ead640a23e6d74dd7a95d4681c549c3
hardikbh <91249866+hardikbh@users.noreply.github.com>
2024-01-12 18:55:47 +0000

    change

--- 01_basics/conversionAndoperation.js
+++ 01_basics/conversionAndoperation.js
@@ -12,7 +12,7 @@ let valueInNumber = Number(score)
 let isLoggedIn=0
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-//console.log(booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
 
 // 1=> true
 // 0=>false
@@ -20,3 +20,27 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "hi"=>true
 
 //console.log(2**3)
+
+//*******operations */
+
+let str1 = "hello"
+let str2 =" hardik"
+let str3 = str1+str2
+// console.log(str3)
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(+true) // conversion of boolean to number using +
+// console.log(+"") // convsersion of string to number 
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
+
+
+
+
+
